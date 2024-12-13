baf7e4afb6763f30644b3b072423a50fdc79942f
Benjamin <benjaminbelloeil03@gmail.com>
2024-12-13 02:18:19 -0600

    add input reference to ControlComponent for improved form handling

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,13 @@
 import {
   Component,
   input,
+  ContentChild,
   ViewEncapsulation,
   HostBinding,
   HostListener,
   inject,
   ElementRef,
+  contentChild,
 } from '@angular/core';
 
 @Component({
@@ -27,9 +29,15 @@ export class ControlComponent {
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+  private control =
+    contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
 
   onClick() {
     console.log('clicked');
     console.log(this.el);
+    console.log(this.control());
   }
 }
