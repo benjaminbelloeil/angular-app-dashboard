d979f86d220c2e6412d00bc0e4eb5936c724e734
Benjamin <benjaminbelloeil03@gmail.com>
2024-12-13 02:00:10 -0600

    implement form submission handling in new tickets component

--- src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css'
 })
 export class NewTicketsComponent {
-
+  onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
+    console.log(title);
+    console.log(ticketText);
+    form.reset();
+  }
 }
